99c467596ece077e03dd0b8f1f313e0c4aa8f151
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-25 15:26:55 -0400

    Implemented first char variable

---
+++ src/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package src.academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        System.out.println(myChar);
+
+
+
+    }
+}
